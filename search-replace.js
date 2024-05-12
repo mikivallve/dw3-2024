01b49501b9f8fcac54529aeffea2fbcd0b83dd23
miki <mvallve@ameba.com.uy>
2024-05-12 18:46:17 -0300

    minifiy copy output

--- search-replace.js
+++ search-replace.js
@@ -1,5 +1,5 @@
 module.exports = {
-    files: 'dist/index.html',
+    files: 'temp/index.html',
     from: [/dist\//g, '<link rel="stylesheet" href="plugin/highlight/monokai.css" />', "plugins: [RevealMarkdown, RevealHighlight, RevealNotes],", '<script src="plugin/highlight/highlight.js"></script>', '<script src="plugin/notes/notes.js"></script>', '<script src="plugin/markdown/markdown.js"></script>'],
     to: '',
 };
