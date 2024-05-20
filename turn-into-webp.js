72c291d1e902e599ca443a8335e6392fe67b09ce
miki <mvallve@ameba.com.uy>
2024-05-20 03:06:49 -0300

    remove jpg and png

--- turn-into-webp.js
+++ turn-into-webp.js
@@ -3,36 +3,33 @@ const path = require('path');
 const sharp = require('sharp');
 
 const assetsDir = './assets';
-const outputDir = './assets';
 
-// Create the output directory if it doesn't exist
-if (!fs.existsSync(outputDir)) {
-    fs.mkdirSync(outputDir);
-}
+function convertImagesToWebP(directory) {
+    fs.readdirSync(directory).forEach(file => {
+        const filePath = path.join(directory, file);
+        const stat = fs.statSync(filePath);
 
-fs.readdir(assetsDir, (err, files) => {
-    if (err) {
-        console.error('Error reading directory:', err);
-        return;
-    }
+        if (stat.isDirectory()) {
+            convertImagesToWebP(filePath); // Recursive call for subdirectories
+        } else {
+            const ext = path.extname(filePath).toLowerCase();
 
-    files.forEach(file => {
-        const filePath = path.join(assetsDir, file);
-        const ext = path.extname(filePath).toLowerCase();
+            if (ext === '.png' || ext === '.jpg' || ext === '.jpeg') {
+                const outputFileName = `${path.basename(file, ext)}.webp`;
+                const outputFilePath = filePath.replace(ext, `.webp`); // Update output file path
 
-        if (ext === '.png' || ext === '.jpg' || ext === '.jpeg') {
-            const outputFileName = `${path.basename(file, ext)}.webp`;
-            const outputFilePath = path.join(outputDir, outputFileName);
-
-            sharp(filePath)
-                .webp()
-                .toFile(outputFilePath, (err, info) => {
-                    if (err) {
-                        console.error(`Conversion of ${file} failed:`, err);
-                    } else {
-                        console.log(`Image ${file} converted successfully to ${outputFileName}`);
-                    }
-                });
+                sharp(filePath)
+                    .webp()
+                    .toFile(outputFilePath, (err, info) => {
+                        if (err) {
+                            console.error(`Conversion of ${file} failed:`, err);
+                        } else {
+                            console.log(`Image ${file} converted successfully to ${outputFileName}`);
+                        }
+                    });
+            }
         }
     });
-});
+}
+
+convertImagesToWebP(assetsDir);
